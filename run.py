90958fe9ac2b08598ce4de90710a791a0dc0f6ce
Oladipin <>
2023-10-16 18:29:16 +0000

    Created instances for ships on the hidden board

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 """
 BOARD_1 is for displaying the ship locations and it is hidden
 while BOARD_2 is for diplaying player guesses and is shown
@@ -20,7 +23,8 @@ def show_board(board):
     print("Guess a row and column to hit")
     print("Row must be between 1-6")
     print("Column must be between A-F")
-    print("=================================\n")
+    print("=================================")
+    print("X = Hit, - = Miss\n")
     print("  A B C D E F")
     print("  #-#-#-#-#-#")
     row_num = 1
@@ -28,4 +32,17 @@ def show_board(board):
         print("%d|%s|" % (row_num, "|".join(row)))
         row_num += 1
 
-show_board(BOARD_1)
+alpha_to_num = {"A":0, "B":1, "C":2, "D":3, "E":4, "F":5,}
+
+
+def create_ships(board):
+    """
+    creates 3 random ship on the hidden board and check to avoid
+    placing ships on the sma spot
+    """
+    for x in range(3):
+        ship_row, ship_col = randint(0, 5), randint(0, 5)
+        while board[ship_row][ship_col] == "X":
+            ship_row, ship_col = randint(0, 5), randint(0, 5)
+        board[ship_row][ship_col] = "X"
+

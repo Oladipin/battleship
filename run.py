e95c869dc986370ba92184a686456ea5758b515b
Oladipin <>
2023-10-16 16:45:43 +0000

    Defined the constant variables

---
+++ run.py
@@ -0,0 +1,10 @@
+"""
+BOARD_1 is for displaying the ship locations and it is hidden
+while BOARD_2 is for diplaying player guesses and is shown
+"""
+BOARD_1 = []
+for x in range(6):
+    BOARD_1.append([" "] * 6)
+BOARD_2 = []
+for x in range(6):
+    BOARD_2.append([" "] * 6)

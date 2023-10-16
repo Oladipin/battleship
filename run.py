c94702a84d1aa6156604e61acd8a1e47aac085c5
Oladipin <>
2023-10-16 17:15:26 +0000

    Create show_board function to display game board

--- run.py
+++ run.py
@@ -8,3 +8,24 @@ for x in range(6):
 BOARD_2 = []
 for x in range(6):
     BOARD_2.append([" "] * 6)
+
+print("******WELCOME TO BATTLESHIP******\n")
+
+
+def show_board(board):
+    """
+    Displays the board for guesses
+    """
+    print("=================================")
+    print("Guess a row and column to hit")
+    print("Row must be between 1-6")
+    print("Column must be between A-F")
+    print("=================================\n")
+    print("  A B C D E F")
+    print("  #-#-#-#-#-#")
+    row_num = 1
+    for row in board:
+        print("%d|%s|" % (row_num, "|".join(row)))
+        row_num += 1
+
+show_board(BOARD_1)
